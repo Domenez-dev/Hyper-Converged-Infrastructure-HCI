40ce6c8909bfb210da7f02c12a914d07fa68130b
Zakkye on Arch <bouzara.zakaria.25@gmail.com>
2026-06-03 14:03:16 +0100

    03/06/26

--- notes/port-forwarding.sh
+++ notes/port-forwarding.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo sysctl -w net.ipv4.ip_forward=1
 
-sudo ip addr add 10.0.0.1/24 dev enp0s20f0u4c2
-sudo ip link set enp0s20f0u4c2 up
+sudo ip addr add 10.0.0.1/24 dev enp0s20f0u3c2
+sudo ip link set enp0s20f0u3c2 up
 
 sudo ip route add 172.16.60.0/24 via 192.168.10.65
 sudo ip route add 172.16.70.0/24 via 192.168.10.65
